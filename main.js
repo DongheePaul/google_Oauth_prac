9b094ea52d0bdc866fa97536981df0fe27636869
DongheePaul <dlehdgml0480@naver.com>
2023-09-03 14:05:45 +0900

    Feat : Add some codes for sign up

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ const app = express();
 
 const GOOGLE_CLIENT_ID = conf.gg.client_id;
 const GOOGLE_CLIENT_SECRET = conf.gg.secret;
-const GOOGLE_REDIRECT_URI = "http://localhost:3000/login/redirect";
+const GOOGLE_LOGIN_REDIRECT_URI = "http://localhost:3000/login/redirect";
+const GOOGLE_SIGNUP_REDIRECT_URI = "http://localhost:3000/signup/redirect";
 
 // 로그인 버튼을 누르면 GET /login으로 이동
 app.get("/", (req, res) => {
   res.send(`
         <h1>Log in</h1>
         <a href="/login">Log in</a>
+        <a href="/signup">Sign up</a>
     `);
 });
 
@@ -21,7 +23,7 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   let url = "https://accounts.google.com/o/oauth2/v2/auth";
   url += `?client_id=${GOOGLE_CLIENT_ID}`;
-  url += `&redirect_uri=${GOOGLE_REDIRECT_URI}`;
+  url += `&redirect_uri=${GOOGLE_LOGIN_REDIRECT_URI}`;
   // 필수 옵션.
   url += "&response_type=code";
   // 구글에 등록된 유저 정보 email, profile을 가져오겠다 명시
@@ -32,6 +34,22 @@ app.get("/login", (req, res) => {
 
 // 구글 계정 선택 화면에서 계정 선택 후 redirect 된 주소
 app.get("/login/redirect", (req, res) => {
+  const { code } = req.query;
+  console.log(`code: ${code}`);
+  res.send("ok");
+});
+
+// 회원가입 라우터
+app.get("/signup", (req, res) => {
+  let url = "https://accounts.google.com/o/oauth2/v2/auth";
+  url += `?client_id=${GOOGLE_CLIENT_ID}`;
+  url += `&redirect_uri=${GOOGLE_SIGNUP_REDIRECT_URI}`;
+  url += "&response_type=code";
+  url += "&scope=email profile";
+  res.redirect(url);
+});
+
+app.get("/signup/redirect", (req, res) => {
   const { code } = req.query;
   console.log(`code: ${code}`);
   res.send("ok");
